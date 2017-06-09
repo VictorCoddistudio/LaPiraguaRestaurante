73cc7619c9c81ee4fe584961df500aafd9afb53f
MarthaCoddistudio <mrhs@gmail.com>
2017-06-09 15:25:27 -0500

    Header view

--- views/header.view.php
+++ views/header.view.php
@@ -1,24 +1,26 @@
 <head>
 	<title>La Piragua Restaurante </title>
- 		
+
 	 <meta name="viewport" content="width=device-width, user-scalable=no,initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"/>
 	 <link rel="shortcut icon" type="image/png" href="images/logomenulapiragua.png" />
 	 <link href='https://fonts.googleapis.com/css?family=Raleway:400,300' rel='stylesheet' type='text/css'>
 	 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+	 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
 	 <link rel="stylesheet" href="css/styles.css">
 	 <link rel="stylesheet" href="css/formulariocontacto.css">
 	<link rel="stylesheet" href="css/estilos.css">
 	 <link rel="stylesheet" href="css/menu.css">
 	 <link rel="stylesheet" href="css/sliderstyles.css">
-	<!-- <link rel="stylesheet" href="css/example.css"/>-->
 	 <link rel="stylesheet" href="css/font-awesome.min.css">
-	
+
 	 <script src="http://code.jquery.com/jquery-latest.js"></script>
 	 <meta charset="UTF-8">
 
-	
+
 	 <script type="text/javascript" src="folletomenu/extras/jquery.min.1.7.js"></script>
 	 <script type="text/javascript" src="folletomenu/extras/modernizr.2.5.3.min.js"></script>
-	</head> 
+	</head>
 	<body>
 	<?php include_once("scripts/analyticstracking.php") ?>
