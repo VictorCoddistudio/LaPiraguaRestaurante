b4c7c3821d54ca5674863fd5ee4bfe0e2899d13f
MarthaCoddistudio <mrhs@gmail.com>
2017-06-12 19:27:15 -0500

    restore menu

--- views/menu.view.php
+++ views/menu.view.php
@@ -1,27 +1,30 @@
 <header>
-		<!--<div id="mostrar-nav"> </div>-->
-		<nav class="navbar navbar-default" role="navigation">
-				<div class="container-fluid">
-					<div class="navbar-header">
-						<button type="button" class="navbar-toggle" dato-toggle="collapse" data-target="#myNavbar">
-							<span class="icon-bar"></span>
-							<span class="icon-bar"></span>
-							<span class="icon-bar"></span>
-						</button>
-					</div>
-					<div class="collapse navbar-collapse" id="myNavbar">
-						<ul class="nav navbar-nav">
-							<li id="logopescado"> <a href="pescadosymariscos.php"><img src="images/logomenupescadosymariscosmenu.png" title="Pescados y Mariscos" alt="Pescados y Mariscos" > </a></li>
-							<li id="logorestaurante"> <a href="index.php"><img src="images/logomenulapiragua.png" title="La Piragua" alt="La Piragua" > </a></li>
-							<li id="logorestaurante"> <a href="index.php"><img src="images/logomenulapiraguaexpress.png" title="La Piragua Express" alt="La Piragua Express" > </a></li>
-							<li id="logohistoria"> <a href="historia.php"><img src="images/iconomenuhistoria.png" title="Historia" alt="Historia" > </a></li>
-							<li id="logomenu"> <a href="menu.php"><img src="images/iconomenumenu.png" title="Menú" alt="Menú" > </a></li>
-							<li id="logobolsatrabajo"> <a href="bolsadetrabajo.php"><img src="images/iconomenubolsadetrabajo.png" title="Bolsa de Trabajo" alt="Bolsa de Trabajo" > </a></li>
-							<li id="logofb"> <a href="https://www.facebook.com/piragua.restaurante/" target="_blank"><img src="images/iconosweb.png" title="Facebook La Piragua" alt="Facebook La Piragua" > </a></li>
-							<li id="logobocontacto"> <a href="contacto.php"><img src="images/iconoswebcontacto.png" title="Contacto" alt="Contacto" > </a></li>
-						</ul>
-					</div>
-			</div>
+		<div id="mostrar-nav"> </div>
+		<nav>
+			<ul>
+				<li class="submenu" id="logopescado"> <a href="pescadosymariscos.php"><img src="images/logomenupescadosymariscosmenu.png" title="Pescados y Mariscos" alt="Pescados y Mariscos" > </a>
+				</li>
+
+				<li class="submenu"	id="logorestaurante"> <a href="index.php"><img src="images/logomenulapiragua.png" title="La Piragua" alt="La Piragua" > </a>
+				</li>
+
+				<li class="submenu"	id="logohistoria"> <a href="historia.php"><img src="images/iconomenuhistoria.png" title="Historia" alt="Historia" > </a>
+				</li>
+
+				<li class="submenu"	id="logomenu"> <a href="menu.php"><img src="images/iconomenumenu.png" title="Menú" alt="Menú" > </a>
+				</li>
+
+				<li class="submenu"	id="logobolsatrabajo"> <a href="bolsadetrabajo.php"><img src="images/iconomenubolsadetrabajo.png" title="Bolsa de Trabajo" alt="Bolsa de Trabajo" > </a>
+				</li>
+
+				<li class="submenu"	id="logofb"> <a href="https://www.facebook.com/piragua.restaurante/" target="_blank"><img src="images/iconosweb.png" title="Facebook La Piragua" alt="Facebook La Piragua" > </a>
+				</li>
+
+				<li class="submenu"	id="logobocontacto"> <a href="contacto.php"><img src="images/iconoswebcontacto.png" title="Contacto" alt="Contacto" > </a>
+				</li>
+			</ul>
+
+
 		</nav>
 
 </header>
