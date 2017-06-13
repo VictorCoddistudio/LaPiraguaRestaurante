94051f84cc7ffded72b184f13b65b3a4d90a4cab
MarthaCoddistudio <mrhs@gmail.com>
2017-06-13 16:14:55 -0500

    index bts

--- views/index.view.php
+++ views/index.view.php
@@ -4,7 +4,7 @@
 <?php require 'header.view.php';?>
 <?php require 'menu2.view.php'; ?>
 
-<div class="container">
+<div class="container-fluid">
   <div class="row">
     <br>
     <div id="slides" class="slidesContainerIndex" >
@@ -19,18 +19,25 @@
     </div>
   </div>
   <div class="row">
-      <a href="historia.php"><img src="images/portada/nuestrahistorialapiraguaicono.png" title="Historia" alt="Nuesta Historia"  class="portada" > </a>
+    <div class="col-lg-4 col-lg-offset-4 col-md-12">
+      <a href="historia.php"><img src="images/portada/nuestrahistorialapiraguaicono.png" title="Historia" alt="Nuesta Historia"  class="img-responsive" > </a>
+    </div>
   </div>
+
   <div class="row">
-    <a href="menu.php"><img src="images/portada/menulapiraguaicono.png" title="Menú" alt="Menú"  class="portada" >  </a>
+    <br>
+    <div class="col-lg-4 col-lg-offset-4 col-md-12">
+      <a href="menu.php"><img src="images/portada/menulapiraguaicono.png" title="Menú" alt="Menú"  class="img-responsive" >  </a>
+    </div>
   </div>
+  <br> <br> <br>
   <div class="row">
-    <div class="col-md-8 col-md-offset-2">
+    <div class="col-lg-8 col-lg-offset-2">
       <img src="images/portada/lapiraguainterior.png" title="La piragua" alt="La piragua" class="img-responsive" ><br/><br/><br/>
     </div>
   </div>
   <div class="row">
-    <div class="col-md-6 col-md-offset-3">
+    <div class="col-lg-6 col-lg-offset-3">
       <div class="embed-responsive embed-responsive-16by9">
         <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/10jZn5PXi0w"></iframe>
       </div>
@@ -38,22 +45,23 @@
   </div>
   <br/><br/><br/>
   <div class="row">
-    <div class="col-md-10 col-md-offset-2">
+    <div class="col-lg-10 col-lg-offset-2 col-md-12">
       <img src="images/portada/kilo-de-chicharron-bassa-la-piragua-restarurante-2017.png" title="Kilo de Chicharrón" alt="Kilo de Chicharrón" class="img-responsive">
     </div>
   </div>
   <br/><br/><br/>
 
   <div class="row">
-    <div class="col-lg-3 col-lg-offset-3 col-md-12 col-sm-12">
+    <div class="col-lg-3 col-lg-offset-3">
       <a href="LaPiraguaExpress.php"><img src="images/portada/piraguaexpress.png" class="img-responsive" title="La Piragua Express" alt="La Piragua Express" > </a>
     </div>
-    <div class="col-lg-3 col-md-12">
+    <div class="col-lg-3">
       <a href="pescadosymariscos.php"><img src="images/portada/pescadosymariscos.png" class="img-responsive" title="Pescados y Mariscos" alt="Pescados y Mariscos" > </a>
     </div>
   </div>
+  <br/><br/><br/>
   <div class="row">
-    <div class="col-md-10 col-md-offset-2">
+    <div class="col-lg-10 col-lg-offset-2 col-md-12">
       <a href="pescadosymariscos.php"><img src="images/portada/pescadosymariscos100natural.png" class="img-responsive" title="La piragua" alt="La piragua"></a>
     </div>
   </div>
