e5ca6576461664ad7812a4aa4430d9dce9aacb37
victorcoddistudio <eduardo.delvallehdz0920@gmail.com>
2017-06-12 20:05:21 -0500

    Mensaje

--- views/menu2.view.php
+++ views/menu2.view.php
@@ -1,15 +1,7 @@
 <header>
-	<nav class="navbar navbar-default" role="navigation">
-			<div class="container-fluid">
-				<div class="navbar-header">
-					<button type="button" class="navbar-toggle" dato-toggle="collapse" data-target="#myNavbar">
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-					</button>
-				</div>
-				<div class="collapse navbar-collapse" id="myNavbar">
-					<ul class="nav navbar-nav">
+	<div id="mostrar-nav"> </div>
+		<nav>
+			<ul>
 					<li id="logopescado2"> <a href="pescadosymariscos.php"><img src="images/logomenupescadosymariscosmenu.png" title="Pescados y Mariscos" alt="Pescados y Mariscos" > </a></li>
 					<li id="logorestaurante2"> <a href="index.php"><img src="images/logomenulapiragua.png" title="La Piragua" alt="La Piragua" > </a></li>
 					<li id="logorestauranteExpress"> <a href="LaPiraguaExpress.php"><img src="images/logomenulapiraguaexpress.png" title="La Piragua Express" alt="La Piragua Express" > </a></li>
@@ -18,9 +10,7 @@
 					<li id="logobolsatrabajo2"> <a href="bolsadetrabajo.php"><img src="images/iconomenubolsadetrabajo.png" title="Bolsa de Trabajo" alt="Bolsa de Trabajo" > </a></li>
 					<li id="logofb2"> <a href="https://www.facebook.com/piragua.restaurante/" target="_blank"><img src="images/iconosweb.png" title="Facebook La Piragua" alt="Facebook La Piragua" ></a></li>
 					<li id="logobocontacto2"> <a href="contacto.php"><img src="images/iconoswebcontacto.png" title="Contacto" alt="Contacto" > </a></li>
-					</ul>
-				</div>
-			</div>
+			</ul>
 		</nav>
 
 </header>
