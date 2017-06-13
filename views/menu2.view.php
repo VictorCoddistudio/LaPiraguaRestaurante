9ecd1ff6a604adcb7e78e558d32adda133f6bf2c
victorcoddistudio <eduardo.delvallehdz0920@gmail.com>
2017-06-12 20:50:27 -0500

    Mensaje

--- views/menu2.view.php
+++ views/menu2.view.php
@@ -4,7 +4,7 @@
 			<ul>
 					<li id="logopescado2"> <a href="pescadosymariscos.php"><img src="images/logomenupescadosymariscosmenu.png" title="Pescados y Mariscos" alt="Pescados y Mariscos" > </a></li>
 					<li id="logorestaurante2"> <a href="index.php"><img src="images/logomenulapiragua.png" title="La Piragua" alt="La Piragua" > </a></li>
-					<li id="logorestauranteExpress"> <a href="LaPiraguaExpress.php"><img src="images/logomenulapiraguaexpress.png" title="La Piragua Express" alt="La Piragua Express" > </a></li>
+					<li id="logorestauranteExpress2"> <a href="LaPiraguaExpress.php"><img src="images/logomenulapiraguaexpress.png" title="La Piragua Express" alt="La Piragua Express" > </a></li>
 					<li id="logohistoria2"> <a href="historia.php"><img src="images/iconomenuhistoria.png" title="Historia" alt="Historia" > </a></li>
 					<li id="logomenu2"> <a href="menu.php"><img src="images/iconomenumenu.png" title="Menú" alt="Menú" > </a></li>
 					<li id="logobolsatrabajo2"> <a href="bolsadetrabajo.php"><img src="images/iconomenubolsadetrabajo.png" title="Bolsa de Trabajo" alt="Bolsa de Trabajo" > </a></li>
